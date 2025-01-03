5decc6b1c117461094d0f75d729a8815312e5348
Alan Prado <alanctprado@gmail.com>
2025-01-03 15:32:52 -0300

    some changes on day 5

--- src/Day05.kt
+++ src/Day05.kt
@@ -3,65 +3,47 @@ import kotlin.math.ceil
 fun main() {
     fun part1(input: List<String>): Int {
         var line: String =""
-        var i = 0
         val set = hashSetOf<String>()
-        while (input[i].isNotBlank()) {
-            set.add(input[i])
-            i++
-        }
-        i++
         var result = 0
-        while (i < input.size) {
-            val numbers = input[i].split(",")
-            var valid = true
-            for (j in numbers.indices) {
-                for (k in j+1..numbers.lastIndex) {
-                    if (!set.contains(numbers[j]+"|"+numbers[k])) {
-                        valid = false
-                        break
+        outer@ for (line in input) {
+            if (line.matches(Regex("""\d+\|\d+"""))) set.add(line)
+            else if (line.isNotBlank()) {
+                val numbers = line.split(",")
+                for (j in numbers.indices) {
+                    for (k in j+1..numbers.lastIndex) {
+                        if (!set.contains(numbers[j]+"|"+numbers[k])) {
+                            continue@outer
+                        }
                     }
                 }
-                if (!valid) break
+                result += numbers[numbers.size/2].toInt()
             }
-            if (valid) result += numbers[numbers.size/2].toInt()
-            i++
         }
         return result
     }
 
     fun part2(input: List<String>): Int {
         var line: String =""
-        var i = 0
         val set = hashSetOf<String>()
-        while (input[i].isNotBlank()) {
-            set.add(input[i])
-            i++
-        }
-        i++
         var result = 0
-        while (i < input.size) {
-            val numbers = input[i].split(",")
-            var valid = true
-            for (j in numbers.indices) {
-                for (k in j+1..numbers.lastIndex) {
-                    if (!set.contains(numbers[j]+"|"+numbers[k])) {
-                        valid = false
-                        break
-                    }
-                }
-                if (!valid) break
-            }
-            if (!valid) {
+        for (line in input) {
+            if (line.matches(Regex("""\d+\|\d+"""))) set.add(line)
+            else if (line.isNotBlank()) {
+                var valid = true
+                val numbers = line.split(",")
                 val count = Array<Int>(numbers.size) { 0 }
                 for (j in numbers.indices) {
                     for (k in numbers.indices) {
-                        if (!set.contains(numbers[j]+"|"+numbers[k])) count[j]++
+                        if (!set.contains(numbers[j] + "|" + numbers[k])) {
+                            if (k > j) valid = false
+                            count[j]++
+                        }
                     }
                 }
+                if (valid) continue
                 val sortedIndices = count.withIndex().sortedBy { it.value }.map { it.index }
-                result += numbers[sortedIndices[sortedIndices.size/2]].toInt()
+                result += numbers[sortedIndices[sortedIndices.size / 2]].toInt()
             }
-            i++
         }
         return result
     }
